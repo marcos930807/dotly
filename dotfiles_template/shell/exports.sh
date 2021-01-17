27ebf869c1e9a40ae68a65166589b1646fea4b9d
Marcos Rodriguez <marcos930807@gmail.com>
2021-01-17 15:07:21 -0500

    Uplaoding export.sh

--- dotfiles_template/shell/exports.sh
+++ dotfiles_template/shell/exports.sh
@@ -1,11 +1,18 @@
-export JAVA_HOME='/Library/Java/JavaVirtualMachines/amazon-corretto-15.jdk/Contents/Home'
+#export JAVA_HOME='/Library/Java/JavaVirtualMachines/amazon-corretto-15.jdk/Contents/Home'
+export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk/Contents/Home'
+#export HOMEBREW_NO_AUTO_UPDATE=1
 export GEM_HOME="$HOME/.gem"
 export GOPATH="$HOME/.go"
+export ANDROID_SDK_ROOTH="/Users/marcos/Documents/SDKs/AndroidSDK"
+export FLUTTER_SDK="/Users/marcos/Documents/SDKs/flutter"
+export FVM_HOME="/Users/marcos/Documents/SDKs/fvm"
 
 export FZF_DEFAULT_OPTS='
   --color=pointer:#ebdbb2,bg+:#3c3836,fg:#ebdbb2,fg+:#fbf1c7,hl:#8ec07c,info:#928374,header:#fb4934
   --reverse
 '
+#Proxy Sttings
+#export https_proxy=mrodriguez:FmYgTCY42342deuBWUabPZ4@gosocketproxy.cloudapp.net:2670
 
 export path=(
   "$HOME/bin"
@@ -23,4 +30,11 @@ export path=(
   "/usr/bin"
   "/usr/sbin"
   "/sbin"
+  "$FLUTTER_SDK/bin"	
+  "$FLUTTER_SDK/bin/cache/dart-sdk/bin"	
+  "$FLUTTER_SDK/.pub-cache/bin"	
+  "$HOME/.pub-cache/bin"
+  "/Users/marcos/Documents/SDKs/AndroidSDK/platform-tools"
+  "/usr/local/share/dotnet"	
+  "/Users/marcos/.dotnet/tools"	
 )
